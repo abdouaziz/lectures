bc788174a50e52a2a9d12f6deaad1feedc51ca28
abdouaziz <abdouaziz@gmail.com>
2021-05-11 23:47:28 +0000

    initializing

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='I group all my lectures in NLP and CV Topic and implementation of papers related ',
+    author='Abdou Aziz DIOP',
+    license='MIT',
+)
